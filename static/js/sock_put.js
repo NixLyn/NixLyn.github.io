2012ed58a9bbffb46cd0af0a19c447959f8f09a9
dillonbreytenach <dillonb_ach@yahoo.com>
2023-03-07 21:53:36 +0200

    cle_sock_

--- static/js/sock_put.js
+++ static/js/sock_put.js
@@ -3,19 +3,6 @@ function validate(){
     console.log('[USER_NAME]:[', user_name, "]");
     for_ip_ = document.getElementById('host').value;
     console.log('[HOST_NAME]:[', for_ip_, "]");
-
-    document.forms["myForm"].value;
 }
 
 
-
-
-
-
-
-
-// <FORM name="myForm" method="POST" action='http://www.example.com/viewdetails.php?id='+how to get scrt_var + '&page=' + how to get the text input; onsubmit="validate();">
-// 
-// <input type="text" id="test" value="">
-// <input type="submit" value="Submit">
-// </form>
