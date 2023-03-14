58f3de1819214601fbef3e5c8f42d007d21709d4
dillonbreytenach <dillonb_ach@yahoo.com>
2023-03-14 21:06:26 +0200

    Meet_team

--- static/js/sock_put.js
+++
@@ -1,8 +0,0 @@
-function validate(){
-    user_name = document.getElementById('fname').value;
-    console.log('[USER_NAME]:[', user_name, "]");
-    for_ip_ = document.getElementById('host').value;
-    console.log('[HOST_NAME]:[', for_ip_, "]");
-}
-
-
